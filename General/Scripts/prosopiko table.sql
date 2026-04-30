58da9ab840af4b2a91ca30b28639688c449e813d
odysseaskikas004-stack <odysseaskikas004@gmail.com>
2026-04-30 13:36:49 +0300

    prosopiko correcetd

--- General/Scripts/prosopiko table.sql
+++ General/Scripts/prosopiko table.sql
@@ -2,8 +2,7 @@ CREATE TABLE Prosopiko (
     amk CHAR(11) PRIMARY KEY, -- Το ΑΜΚ είναι το PK 
     ID_atomou INT NOT NULL,   -- Το FK που δείχνει στον πίνακα Άτομα
     imerominia_proslipsis DATE NOT NULL,
-   typos_prosopikou VARCHAR(50) NOT NULL CHECK (typos_prosopikou IN ('Ιατρός', 'Νοσηλευτής', 'Διοικητικό')),	--ENUM ειναι μια λιστα με της παρακατω επιλογες
-    																		--αν βαλω καποια αλλη λεξη η βαση την απορριπτει
+   typos_prosopikou VARCHAR(50) NOT NULL CHECK (typos_prosopikou IN ('Ιατρός', 'Νοσηλευτής', 'Διοικητικό')),	
     -- Σύνδεση με τον πίνακα Άτομα
     FOREIGN KEY (ID_atomou) REFERENCES Atoma(ID_atomou) ON DELETE CASCADE --το ID atomou ειναι FK που δειχνει στον πινακα Atoma
 ) ENGINE=InnoDB;										--ON DELETE CASCADE: αν διαγραψω στοιχειο απο πινακα Atoma, θα διαγραφει αυτοματα και απο τον prosopiko
