bc9f22b57fc63b86be50bdabd14515ac812ca2b6
odysseaskikas004-stack <odysseaskikas004@gmail.com>
2026-05-01 12:24:45 +0300

    klines nosileutes

---
+++ General/Scripts/Klines table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Klines (
+    ID_klinis INT AUTO_INCREMENT PRIMARY KEY, -- Μοναδικός αριθμός κλίνης
+    typos VARCHAR(50) NOT NULL 
+        CHECK (typos IN ('ΜΕΘ', 'μονόκλινο', 'πολύκλινο')),
+    katastasi VARCHAR(50) NOT NULL 
+        CHECK (katastasi IN ('διαθέσιμη', 'κατειλημμένη', 'υπό συντήρηση')),
+    ID_tmhmatos INT NOT NULL,
+    
+    CONSTRAINT fk_klinis_tmhma 
+        FOREIGN KEY (ID_tmhmatos) 
+        REFERENCES Tmhmata(ID_tmhmatos) 
+        ON DELETE CASCADE
+) ENGINE=InnoDB;
---
+++ General/Scripts/nosileftes table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Nosileytes (
+    amk CHAR(11) PRIMARY KEY,
+    va8mida VARCHAR(50) NOT NULL 
+        CHECK (va8mida IN ('Βοηθός Νοσηλευτή', 'Νοσηλευτής', 'Προϊστάμενος')),
+    ID_tmhmatos INT NOT NULL,
+    
+    CONSTRAINT fk_nosileytis_prosopiko 
+        FOREIGN KEY (amk) 
+        REFERENCES Prosopiko(amk) 
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_nosileytis_tmhma 
+        FOREIGN KEY (ID_tmhmatos) 
+        REFERENCES Tmhmata(ID_tmhmatos) 
+        ON DELETE RESTRICT
+) ENGINE=InnoDB;
