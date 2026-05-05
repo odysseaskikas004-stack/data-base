fefa100b85c49c2092735b2f5504a9893995e59b
Βασίλης <@10.255.220.146>
2026-05-05 14:20:38 +0300

    efhmeries new

--- General/Scripts/Efhmeries.sql
+++ General/Scripts/Efhmeries.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Efhmeries (
     id_efhmerias INT AUTO_INCREMENT PRIMARY KEY,
     id_vardias INT NOT NULL,
-    amk_proswpikou VARCHAR(11) NOT NULL,
+    amka_prosopikou CHAR(11) NOT NULL,
     -- Μοναδικός συνδυασμός: Ένα άτομο δεν μπορεί να δηλωθεί δύο φορές στην ίδια βάρδια
-    UNIQUE (id_vardias, amk_proswpikou),
+    UNIQUE (id_vardias, amka_prosopikou),
     FOREIGN KEY (id_vardias) REFERENCES Vardies(id_vardias) ON DELETE CASCADE,
-    FOREIGN KEY (amk_proswpikou) REFERENCES Proswpiko(amk) ON DELETE CASCADE
+    FOREIGN KEY (amka_prosopikou) REFERENCES Prosopiko(amka) ON DELETE CASCADE
 ) ENGINE=InnoDB;
