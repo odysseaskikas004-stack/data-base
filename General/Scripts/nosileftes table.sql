c89ae962dcc3d9faf25ca7ab30f7e9301eaed134
Βασίλης <@10.255.220.146>
2026-05-05 14:29:39 +0300

    nosileftes new

--- General/Scripts/nosileftes table.sql
+++ General/Scripts/nosileftes table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Nosileytes (
-    amk CHAR(11) PRIMARY KEY,
+    amka CHAR(11) PRIMARY KEY,
     va8mida VARCHAR(50) NOT NULL 
         CHECK (va8mida IN ('Βοηθός Νοσηλευτή', 'Νοσηλευτής', 'Προϊστάμενος')),
     ID_tmhmatos INT NOT NULL,
     
     CONSTRAINT fk_nosileytis_prosopiko 
-        FOREIGN KEY (amk) 
-        REFERENCES Prosopiko(amk) 
+        FOREIGN KEY (amka) 
+        REFERENCES Prosopiko(amka) 
         ON DELETE CASCADE,
         
     CONSTRAINT fk_nosileytis_tmhma 
