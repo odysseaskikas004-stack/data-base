770b8047bcdc578f758366e95d00f0d32713f69b
Βασίλης <@10.255.220.146>
2026-05-05 14:17:21 +0300

    dioikitiko new

--- General/Scripts/Dioikitiko prosopiko table.sql
+++ General/Scripts/Dioikitiko prosopiko table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Dioikitiko_Prosopiko (
-    amk CHAR(11) PRIMARY KEY,
+    amka CHAR(11) PRIMARY KEY,
     ka8ikonta_rolos VARCHAR(100) NOT NULL,
     grafeio VARCHAR(50) NOT NULL,
     ID_tmhmatos INT NOT NULL,
     
     CONSTRAINT fk_dioikitiko_prosopiko 
-        FOREIGN KEY (amk) 
-        REFERENCES Prosopiko(amk) 
+        FOREIGN KEY (amka) 
+        REFERENCES Prosopiko(amka) 
         ON DELETE CASCADE,
         
     CONSTRAINT fk_dioikitiko_tmhma 
