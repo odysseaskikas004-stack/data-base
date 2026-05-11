3dbf0f6491f2841d1efe4421308a785dd89b8557
user <user@Bill-NB>
2026-05-11 13:27:03 +0300

    k

---
+++ General/Scripts/Iatroi nosileias.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Iatroi_nosileias (
+    ID_nosileias INT NOT NULL,
+    AMKA_iatrou CHAR(11) NOT NULL,
+    
+    PRIMARY KEY (ID_nosileias, AMKA_iatrou),
+    
+    CONSTRAINT fk_in_nosileia 
+        FOREIGN KEY (ID_nosileias) 
+        REFERENCES Nosileies(ID_nosileias) 
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_in_iatros 
+        FOREIGN KEY (AMKA_iatrou) 
+        REFERENCES Iatroi(amka) 
+        ON DELETE CASCADE
+) ENGINE=InnoDB;
