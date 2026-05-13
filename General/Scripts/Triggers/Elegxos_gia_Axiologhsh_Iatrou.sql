a860ef0c1e884cbb50f7ae8f1018774db054604f
odysseaskikas004-stack <odysseaskikas004@gmail.com>
2026-05-13 18:48:38 +0300

    check

--- General/Scripts/Triggers/Elegxos_gia_Axiologhsh_Iatrou.sql
+++ General/Scripts/Triggers/Elegxos_gia_Axiologhsh_Iatrou.sql
@@ -5,6 +5,7 @@ BEFORE INSERT ON Axiologisi_Iatrwn
 FOR EACH ROW
 BEGIN
     DECLARE v_exit_date DATE;
+	DECLARE v_participation_count INT;
 
     SELECT imerominia_exodou INTO v_exit_date 
     FROM Nosileies 
@@ -13,6 +14,23 @@ BEGIN
     IF v_exit_date IS NULL THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Σφάλμα: Η αξιολόγηση ιατρού επιτρέπεται μόνο μετά την ολοκλήρωση της νοσηλείας.';
+    END IF;
+    
+    -- 2. Έλεγχος αν ο γιατρός συμμετείχε όντως στη νοσηλεία
+    SELECT COUNT(*) INTO v_participation_count
+    FROM Iatrikes_Praxeis 
+    WHERE ID_nosileias = NEW.ID_nosileias AND AMKA = NEW.AMKA_iatrou;
+
+    -- (Προαιρετικά) Έλεγχος και στις συνταγογραφήσεις αν δεν βρέθηκε στις πράξεις
+    IF v_participation_count = 0 THEN
+        SELECT COUNT(*) INTO v_participation_count
+        FROM Agwges 
+        WHERE ID_nosileias = NEW.ID_nosileias AND AMKA_iatrou = NEW.AMKA_iatrou;
+    END IF;
+
+    IF v_participation_count = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Σφάλμα: Μπορείτε να αξιολογήσετε μόνο γιατρούς που συμμετείχαν στη φροντίδα σας.';
     END IF;
 END; //
 
--- General/Scripts/Triggers/Periorismoi_Epopteias.sql
+++ General/Scripts/Triggers/Periorismoi_Epopteias.sql
@@ -1,10 +1,16 @@
 DELIMITER //
 
--- Trigger για τον έλεγχο κατά την ΕΙΣΑΓΩΓΗ (INSERT)
+-- 1. TRIGGER ΓΙΑ INSERT
 CREATE TRIGGER trg_iatroi_supervision_insert
 BEFORE INSERT ON Iatroi
 FOR EACH ROW
 BEGIN
+    -- Α απαγόρευση αυτο-εποπτείας
+    IF NEW.amka_epopti = NEW.amka THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Σφάλμα: Ένας ιατρός δεν μπορεί να εποπτεύει τον εαυτό του.';
+    END IF;
+
     -- Οι ειδικευόμενοι έχουν υποχρεωτικά επόπτη
     IF NEW.va8mida = 'Ειδικευόμενος' AND NEW.amka_epopti IS NULL THEN
         SIGNAL SQLSTATE '45000' 
@@ -16,19 +22,44 @@ BEGIN
         SIGNAL SQLSTATE '45000' 
         SET MESSAGE_TEXT = 'Σφάλμα: Οι Διευθυντές δεν επιτρέπεται να έχουν επόπτη.';
     END IF;
-END; //
 
--- Trigger για τον έλεγχο κατά την ΕΝΗΜΕΡΩΣΗ (UPDATE) - Αποφυγή Κυκλικής Εποπτείας
-CREATE TRIGGER trg_iatroi_supervision_update
-BEFORE UPDATE ON Iatroi
-FOR EACH ROW
-BEGIN
+    -- Έλεγχος κυκλικής αλυσίδας 
     IF NEW.amka_epopti IS NOT NULL THEN
-        -- Έλεγχος κυκλικής αλυσίδας (Α επόπτης του Β, Β επόπτης του Α)
         IF EXISTS (SELECT 1 FROM Iatroi WHERE amka = NEW.amka_epopti AND amka_epopti = NEW.amka) THEN
             SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Σφάλμα: Απαγορεύεται η άμεση κυκλική αλυσίδα εποπτείας.';
         END IF;
     END IF;
 END; //
+
+
+-- 2. TRIGGER ΓΙΑ UPDATE
+CREATE TRIGGER trg_iatroi_supervision_update
+BEFORE UPDATE ON Iatroi
+FOR EACH ROW
+BEGIN
+    -- Απαγόρευση αυτο-εποπτείας
+    IF NEW.amka_epopti = NEW.amka THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Σφάλμα: Ένας ιατρός δεν μπορεί να εποπτεύει τον εαυτό του.';
+    END IF;
+
+    IF NEW.va8mida = 'Ειδικευόμενος' AND NEW.amka_epopti IS NULL THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Σφάλμα: Οι ειδικευόμενοι πρέπει υποχρεωτικά να έχουν επόπτη.';
+    END IF;
+
+    IF NEW.va8mida = 'Διευθυντής' AND NEW.amka_epopti IS NOT NULL THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Σφάλμα: Οι Διευθυντές δεν επιτρέπεται να έχουν επόπτη.';
+    END IF;
+
+    IF NEW.amka_epopti IS NOT NULL THEN
+        IF EXISTS (SELECT 1 FROM Iatroi WHERE amka = NEW.amka_epopti AND amka_epopti = NEW.amka) THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Σφάλμα: Απαγορεύεται η άμεση κυκλική αλυσίδα εποπτείας.';
+        END IF;
+    END IF;
+END; //
+
 DELIMITER ;
