93d79625a0e5cc38622eec4fd78476fe1d659212
vasilis <user@Bill-NB>
2026-05-16 20:25:18 +0300

    Εργαστηριακες Εξετάσεις

---
+++ General/Scripts/Tables/Ergasthriakes_Exetaseis.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Ergasthriakes_Exetaseis (
+    kwdikos_exetashs VARCHAR(50) PRIMARY KEY,            
+    hmeromhnia DATETIME NOT NULL,          
+    typos_exetashs VARCHAR(100),                                         
+    kostos DECIMAL(10, 2) NOT NULL,        
+    apotelesma TEXT,                                            
+    AMKA CHAR(11),                        
+    ID_nosileias INT NOT NULL,            
+
+
+    CONSTRAINT fk_praxi_iatros 
+        FOREIGN KEY (AMKA) 
+        REFERENCES Iatroi(amka) 
+        ON DELETE RESTRICT,
+
+    CONSTRAINT fk_nosileia_praxi 
+        FOREIGN KEY (ID_nosileias) 
+        REFERENCES Nosileies(ID_nosileias) 
+        ON DELETE CASCADE
+        
+
+) ENGINE=InnoDB;
