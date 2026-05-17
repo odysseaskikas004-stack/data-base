085cf857cc82e44a2515a43213f02686cd63c271
vasilis <user@Bill-NB>
2026-05-17 15:53:41 +0300

    Removed comments

--- General/Scripts/Tables/Prosopiko_Table.sql
+++ General/Scripts/Tables/Prosopiko_Table.sql
@@ -2,5 +2,5 @@ CREATE TABLE Prosopiko (
     amka CHAR(11) PRIMARY KEY,
     imerominia_proslipsis DATE NOT NULL,
     typos_prosopikou VARCHAR(50) NOT NULL CHECK (typos_prosopikou IN ('Ιατρός', 'Νοσηλευτής', 'Διοικητικό')),	
-    FOREIGN KEY (amka) REFERENCES Atoma(amka) ON DELETE CASCADE --το ID atomou ειναι FK που δειχνει στον πινακα Atoma
-) ENGINE=InnoDB;										--ON DELETE CASCADE: αν διαγραψω στοιχειο απο πινακα Atoma, θα διαγραφει αυτοματα και απο τον prosopiko
+    FOREIGN KEY (amka) REFERENCES Atoma(amka) ON DELETE CASCADE 
+) ENGINE=InnoDB;
