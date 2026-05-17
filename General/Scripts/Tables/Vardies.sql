9086825b5e775cfeb82f8d07e3956e6e5c802d5f
i-will-come-up-with-something-different <lakushi.daniel@gmail.com>
2026-05-17 15:58:53 +0300

    Update Vardies.sql

--- General/Scripts/Tables/Vardies.sql
+++ General/Scripts/Tables/Vardies.sql
@@ -2,7 +2,9 @@ CREATE TABLE Vardies (
     id_vardias INT AUTO_INCREMENT PRIMARY KEY,
     id_tmhmatos INT NOT NULL,
     hmeromhnia DATE NOT NULL,
-    typos_vardias ENUM('Πρωινή', 'Απογευματινή', 'Νυχτερινή') NOT NULL,
-    UNIQUE (id_tmhmatos, hmeromhnia, typos_vardias),
-    FOREIGN KEY (id_tmhmatos) REFERENCES Tmhmata(id_tmhmatos) ON DELETE CASCADE
+    typos_vardias VARCHAR(20) NOT NULL 
+        CHECK (typos_vardias IN ('Πρωινή', 'Απογευματινή', 'Νυχτερινή')),
+        
+    CONSTRAINT uq_vardia_tmhma UNIQUE (id_tmhmatos, hmeromhnia, typos_vardias),
+    CONSTRAINT fk_vardia_tmhma FOREIGN KEY (id_tmhmatos) REFERENCES Tmhmata(id_tmhmatos) ON DELETE CASCADE
 ) ENGINE=InnoDB;
