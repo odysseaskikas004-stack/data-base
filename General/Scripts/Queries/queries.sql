e54b097652d4087677f3cbd2cf33068a3f48f5cf
odysseaskikas004-stack <odysseaskikas004@gmail.com>
2026-05-29 12:13:30 +0300

    typos

--- General/Scripts/Queries/queries.sql
+++ General/Scripts/Queries/queries.sql
@@ -6,7 +6,7 @@
 SELECT 
     t.onoma_tmhmatos,
     YEAR(n.imerominia_eisagwgis) AS etos,
-    k.kwdikos_KEN,
+    k.kodikos_KEN,
     a.asfalistikos_foreas,
     SUM(k.basiko_kostos) AS esoda_basika,
     SUM(kos.paratasi_nosileias * 100) AS esoda_prosth_xreosis, -- Υπόθεση: 100€/μέρα για υπέρβαση Μεγ.Διαρκ.Νοσιλ.
@@ -14,14 +14,14 @@ SELECT
 FROM Nosileies n
 JOIN Tmhmata t ON n.ID_tmhmatos = t.ID_tmhmatos
 JOIN Kostologisi kos ON n.ID_nosileias = kos.ID_nosileias
-JOIN KEN k ON kos.kwdikos_KEN = k.kwdikos_KEN
+JOIN KEN k ON kos.kodikos_KEN = k.kodikos_KEN
 JOIN As8eneis a ON n.AMKA_astheni = a.amka
-GROUP BY t.onoma_tmhmatos, etos, k.kwdikos_KEN, a.asfalistikos_foreas;
+GROUP BY t.onoma_tmhmatos, etos, k.kodikos_KEN, a.asfalistikos_foreas;
 
 -- Q2: Ιατροί συγκεκριμένης ειδικότητας και επεμβάσεις [cite: 81]
 SELECT 
     i.amka,
-    at.epwnymo,
+    at.eponymo,
     at.onoma,
     CASE 
         WHEN EXISTS (
@@ -39,7 +39,7 @@ GROUP BY i.amka;
 
 -- Q3: Ασθενείς με >3 νοσηλείες στο ίδιο τμήμα [cite: 82]
 SELECT 
-    at.epwnymo, at.onoma, t.onoma_tmhmatos, 
+    at.eponymo, at.onoma, t.onoma_tmhmatos, 
     COUNT(n.ID_nosileias) AS ari8mos_nosileiwn,
     SUM(k.synoliko_kostos) AS synoliko_kostos
 FROM Nosileies n
@@ -63,7 +63,7 @@ GROUP BY i.amka;
 
 -- Q5: Νέοι ιατροί (<35 ετών) με τις περισσότερες επεμβάσεις
 SELECT 
-    at.epwnymo, at.onoma, at.hlikia,
+    at.eponymo, at.onoma, at.hlikia,
     COUNT(ip.kwdikos_praxis) AS synolo_epembasewn
 FROM Iatroi i
 JOIN Atoma at ON i.amka = at.AMKA
@@ -86,7 +86,7 @@ WHERE n.AMKA_astheni = '09876543210'; -- Παράδειγμα AMKA
 SELECT 
     do.onoma_ousias,
     COUNT(DISTINCT aa.amka) AS ari8mos_allergikwn,
-    COUNT(DISTINCT sf.kwdikos_farmakou) AS ari8mos_farmakwn
+    COUNT(DISTINCT sf.kodikos_farmakou) AS ari8mos_farmakwn
 FROM Drastikes_Ousies do
 LEFT JOIN Allergies_As8enwn aa ON do.ID_ousias = aa.id_ousias
 LEFT JOIN Synthesi_Farmakou sf ON do.ID_ousias = sf.ID_ousias
@@ -94,7 +94,7 @@ GROUP BY do.ID_ousias
 ORDER BY ari8mos_allergikwn DESC;
 
 -- Q8: Προσωπικό χωρίς εφημερία σε συγκεκριμένη ημερομηνία/τμήμα 
-SELECT at.epwnymo, at.onoma, p.typos_prosopikou
+SELECT at.eponymo, at.onoma, p.typos_prosopikou
 FROM Prosopiko p
 JOIN Atoma at ON p.amka = at.AMKA
 WHERE p.amka NOT IN (
@@ -132,23 +132,23 @@ HAVING count_epembaseis <= (
 
 -- Q12: Απαιτούμενο προσωπικό ανά τμήμα/βάρδια για εβδομάδα 
 -- Εξαρτάται από τον πίνακα (Vardies/Efhmeries)
-SELECT t.onoma_tmhmatos, v.typos_bardias, p.typos_prosopikou, COUNT(*) AS proswpiko_count
+SELECT t.onoma_tmhmatos, v.typos_vardias, p.typos_prosopikou, COUNT(*) AS proswpiko_count
 FROM Efhmeries ef
 JOIN Vardies v ON ef.ID_bardias = v.ID_bardias
 JOIN Tmhmata t ON ef.ID_tmhmatos = t.ID_tmhmatos
 JOIN Prosopiko p ON ef.AMKA = p.amka
 WHERE v.imerominia BETWEEN '2026-05-18' AND '2026-05-24'
-GROUP BY t.ID_tmhmatos, v.typos_bardias, p.typos_prosopikou;
+GROUP BY t.ID_tmhmatos, v.typos_vardias, p.typos_prosopikou;
 
 -- Q13: Ιεραρχία εποπτείας ιατρού 
 WITH RECURSIVE SupervisionHierarchy AS (
-    SELECT amka, AMK_epopti, 1 AS level
+    SELECT amka, amka_epopti, 1 AS level
     FROM Iatroi
     WHERE amka = '12345678901' -- ΑΜΚΑ εκκίνησης
     UNION ALL
-    SELECT i.amka, i.AMK_epopti, sh.level + 1
+    SELECT i.amka, i.amka_epopti, sh.level + 1
     FROM Iatroi i
-    INNER JOIN SupervisionHierarchy sh ON i.amka = sh.AMK_epopti
+    INNER JOIN SupervisionHierarchy sh ON i.amka = sh.amka_epopti
 )
 SELECT * FROM SupervisionHierarchy;
 
